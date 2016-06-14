453139841d0f29302c9527859fa061c064bdfb5e
amreo <amreo@live.it>
2016-06-14 16:09:10 +0200

    Fusi i due src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,8 +1,21 @@
-<<<<<<< HEAD
 #include "SPI.h"
 #include "Ethernet.h"
 #include "sha1.h"
 #include "mysql.h"
+#define PIN_INPUT_IR = 5;
+
+
+/*Nexapp project : When the sensor notice a box arduino modify the infrared value
+in true then the barcode scanner read the barcode on the box, arduino send it to
+the MySQL database that compare it with the barcodes saved in its memory, if the 
+barcode is valid arduino modify the value of producted items on the database; there
+are different kinds of products so every barcode is relative to a product.
+If the sensor notice a box but the barcode is not readable or isn't in the database
+the program will notify an error*/
+
+//variable for read the return of the database
+boolean found = false; 
+byte irState;
 
 byte mac[] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 }; //TODO: impostare mac address corretto
 IPAddress ip(11,22,33,44); //TODO: impostare ip di arduino
@@ -22,9 +35,11 @@ const char updateQueryModel[] = "UPDATE dati_produzione.output_catena SET numPro
 //Funzione che instaura la connessione con il server mysql
 void stabilisciConnessione()
 {
+	//si connette al database
 	if (my_conn.mysql_connect(server_addr, 3306, user, password)) 
 	{
 		Serial.println("OK!");
+		//invia una query di test	
 		if (my_conn.cmd_query(logQuery))
 		{
 			Serial.println("Query inviata con successo");
@@ -34,6 +49,7 @@ void stabilisciConnessione()
 	} 
 	else 
 	{	 	
+		//blocca l'arduino
 		while (true) {
 			Serial.println("Fallito.");
 		}
@@ -44,8 +60,11 @@ void stabilisciConnessione()
 //Funzione che invia un messaggio di log al server mysql
 void sendLog(int posizione, char messaggio[])
 {
+	//stringa temporanea
 	char query[128]; //Lunghezza max della query
+	//sostituisce %d e %s con la relativa parte		
 	sprintf(query, logQueryModel, posizione, messaggio);
+	//invia stringa		
 	if (my_conn.cmd_query(query))
 	{
 		Serial.println("Query inviata con successo");
@@ -58,8 +77,11 @@ void sendLog(int posizione, char messaggio[])
 //Funzione che incrementa il numero di oggetti prodotti all'ID/Barcode
 void sendProductUpdate(int barcode)
 {
+	//stringa temporanea
 	char query[128]; //Lunghezza max della query
+	//compone la query
 	sprintf(query, updateQueryModel, barcode);
+	//la invia al database
 	if (my_conn.cmd_query(query))
 	{
 		Serial.print("Query inviata con successo: ");
@@ -72,6 +94,7 @@ void sendProductUpdate(int barcode)
 
 void setup()
 {
+	pinMode(PIN_INPUT_IR, INPUT);
 	Serial.begin(9600);
 	Ethernet.begin(mac, ip);
 
@@ -80,67 +103,37 @@ void setup()
 	Serial.println("Connettendo...");
 	stabilisciConnessione();
 }
-
 void loop()
 {
-=======
-/*Nexapp project : When the sensor notice a box arduino modify the infrared value
-in true then the barcode scanner read the barcode on the box, arduino send it to
-the MySQL database that compare it with the barcodes saved in its memory, if the 
-barcode is valid arduino modify the value of producted items on the database; there
-are different kinds of products so every barcode is relative to a product.
-If the sensor notice a box but the barcode is not readable or isn't in the database
-the program will notify an error*/
-
-//variable for read the return of the database
-boolean found = false;
-
-const int outputIR = 5; 
-
-
->>>>>>> e8ee3e78f3369183302368fb95ce39dde0d16715
-
-
-void setup()
-{
-	Serial.begin(9600);
-	pinMode(outputIR, INPUT);
-	
-}
-
-void loop()
-{
- digitalRead(outputIR);
- if (outputIR == LOW)
- {
- 	
- 	// read barcode by the scanner
- 	/*........................*/
- 	
-    // send barcode to the database
- 	/*........................*/
- 	
- 	 // if the barcode is in the database found variable becomes true
- 	/* found = true; */
- 	
- 	//if the barcode hasn't been found the found variable keep false
- 	
- 	if(found == true)
+	irState = digitalRead(outputIR);
+ 	if (irState == LOW)
  	{
- 		/*ask to the database the kind of product corresponding to the barcode*/
- 		/*....................*/
- 		
- 		/*modify the number of items produced*/
- 		/*..................*/
- 		
- 	}
  	
- 	else
- 	{
- 		/*send an error*/
+ 		// read barcode by the scanner
  		/*........................*/
- 	}
- }
  	
- 
+	    	// send barcode to the database
+	 	/*........................*/
+ 	
+ 		// if the barcode is in the database found variable becomes true
+ 		/* found = true; */
+ 	
+ 		//if the barcode hasn't been found the found variable keep false
+ 	
+	 	if(found == true)
+ 		{
+ 			/*ask to the database the kind of product corresponding to the barcode*/
+ 			/*....................*/
+ 			
+ 			/*modify the number of items produced*/
+ 			/*..................*/
+ 		
+ 		}
+ 	
+	 	else
+	 	{
+	 		/*send an error*/
+	 		/*........................*/
+	 	}
+	 } 	
 }
