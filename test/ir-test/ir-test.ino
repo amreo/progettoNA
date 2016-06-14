fc0f4c4995bec66a9b2ca1d306ac723c7062f58d
amreo <amreo@live.it>
2016-06-14 17:17:54 +0200

    Fixato errori sintattici ir-test

--- test/ir-test/ir-test.ino
+++ test/ir-test/ir-test.ino
@@ -1,27 +1,21 @@
 /* Skecth for verify the behavior of the ir sensor*/
+#define PIN_OUTPUT_IR 5 
 
-const int outputIR = 5; 
+byte irState;
 
 void setup()
 {
 	Serial.begin(9600);
-	pinMode(outputIR, INPUT);
-	
+	pinMode(PIN_OUTPUT_IR, INPUT);
 }
 
 void loop()
 {
- 
- Serial.println(digitalRead(outputIR));
- 
- digitalRead(outputIR);
- 
- if (outputIR == LOW)
- {
- 	
- Serial.println("Pacco rilevato");
- 
- }
- 	
- 
+ 	irState = digitalRead(PIN_OUTPUT_IR);
+	Serial.println(irState); 
+
+ 	if (irState == LOW)
+ 	{
+ 		Serial.println("Pacco rilevato");
+ 	}
 }
