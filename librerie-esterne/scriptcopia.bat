969482211a040a4e506c092ef510d4c81443a7ed
amreo <amreo@live.it>
2016-06-15 15:12:34 +0200

    Corretto destinazione

--- librerie-esterne/scriptcopia.bat
+++ librerie-esterne/scriptcopia.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-XCOPY PS2Keyboard/PS2Keyboard ~/sketchbook/libraries %user%/Documenti/sketchbook/libraries /e
-XCOPY  mysql_connector_arduino-1.0.3rc/* ~/sketchbook/libraries %user%/Documenti/sketchbook/libraries /e
+XCOPY PS2Keyboard/PS2Keyboard %user%/Documenti/sketchbook/libraries /e
+XCOPY  mysql_connector_arduino-1.0.3rc/* %user%/Documenti/sketchbook/libraries /e
