0650ddb2fdd624053fb4642cac2fd5ca90781409
kevinpre <kevin.presto@hotmail.it>
2016-06-15 09:22:52 +0200

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,10 +1,3 @@
-#include "SPI.h"
-#include "Ethernet.h"
-#include "sha1.h"
-#include "mysql.h"
-#define PIN_INPUT_IR 5
-
-
 /*Nexapp project : When the sensor notice a box arduino modify the infrared value
 in true then the barcode scanner read the barcode on the box, arduino send it to
 the MySQL database that compare it with the barcodes saved in its memory, if the 
@@ -12,6 +5,14 @@ barcode is valid arduino modify the value of producted items on the database; th
 are different kinds of products so every barcode is relative to a product.
 If the sensor notice a box but the barcode is not readable or isn't in the database
 the program will notify an error*/
+
+
+#include "SPI.h"
+#include "Ethernet.h"
+#include "sha1.h"
+#include "mysql.h"
+#define PIN_INPUT_IR 5
+
 
 //variable for read the return of the database
 boolean found = false; 
