add8c9d740411100828f5589d522767aa6c0ca33
kevinpre <kevin.presto@hotmail.it>
2016-06-20 21:18:59 +0200

    Update update.php

--- Impostazioni/Sito Modifica Remoto/update.php
+++ Impostazioni/Sito Modifica Remoto/update.php
@@ -1,11 +1,11 @@
 <?php
 mysql_connect("Host Name", "User Name", "User Password") or die("Connection Failed");
 mysql_select_db("DataBase Name")or die("Connection Failed");
-$arduinoid = $_POST['arduinoid'];
+$idstazione = $_POST['idstazione'];
 $lineaproduzione = $_POST['lineaproduzione'];
 $timeout = $_POST['timeout'];
 $descrizione = $_POST['descrizione'];
-$query = "UPDATE test SET lineaproduzione = '$lineaproduzione' timeout = '$timeout'  descrizione= '$descrizione' WHERE name = '$arduinoid'";
+$query = "UPDATE test SET lineaproduzione = '$lineaproduzione' timeout = '$timeout'  descrizione= '$descrizione' WHERE name = '$idstazione'";
 if(mysql_query($query)){
 echo "updated";}
 else{
