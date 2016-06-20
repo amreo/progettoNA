c0a4e87f7d2a4f9d0ff5d4d30eac502197d0d137
kevinpre <kevin.presto@hotmail.it>
2016-06-20 21:09:00 +0200

    Update settings.sql

--- Impostazioni/settings.sql
+++ Impostazioni/settings.sql
@@ -5,6 +5,3 @@ lineaproduzione INTEGER,
 barcodetimeout INTEGER,
 descrizione TEXT,
 );
-
-INSERT INTO settings (arduinoid, lineaproduzione, barcodetimeout,descrizione)
-VALUES ('arduino','arduino4you',3306,'192.168.1.101',3,'11 22 33 44 55 66', '192.168.1.105', 1, 5000);
