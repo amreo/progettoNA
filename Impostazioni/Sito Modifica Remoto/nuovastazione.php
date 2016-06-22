3c99f53f69ba42f8ffbe19d317e7f3090bdcafa8
root <root@debian>
2016-06-22 12:54:00 +0200

    Fixato alcuni errori

--- Impostazioni/Sito Modifica Remoto/nuovastazione.php
+++ Impostazioni/Sito Modifica Remoto/nuovastazione.php
@@ -1,6 +1,6 @@
 <?php
-mysql_connect("Host Name", "User Name", "User Password") or die("Connection Failed");
-mysql_select_db("DataBase Name")or die("Connection Failed");
+mysql_connect("127.0.0.1", "root", "PASSWORD") or die("Connection Failed");
+mysql_select_db("dati_produzione")or die("Connection Failed");
 $idstazione = $_POST['idstazione'];
 $lineaproduzione = $_POST['lineaproduzione'];
 $timeout = $_POST['timeout'];
--- Impostazioni/Sito Modifica Remoto/update.php
+++ Impostazioni/Sito Modifica Remoto/update.php
@@ -1,11 +1,11 @@
 <?php
-mysql_connect("Host Name", "User Name", "User Password") or die("Connection Failed");
-mysql_select_db("DataBase Name")or die("Connection Failed");
+mysql_connect("127.0.0.1", "root", "PASSWORD") or die("Connection Failed");
+mysql_select_db("dati_produzione")or die("Connection Failed");
 $idstazione = $_POST['idstazione'];
 $lineaproduzione = $_POST['lineaproduzione'];
 $timeout = $_POST['timeout'];
 $descrizione = $_POST['descrizione'];
-$query = "UPDATE test SET Lineaproduzione = '$lineaproduzione',Barcodetimeout = '$timeout',Descrizione= '$descrizione',WHERE IDStazione = '$idstazione'";
+$query = "UPDATE settings (IDStazione, Lineaproduzione, Barcodetimeout, Descrizione) SET Lineaproduzione = $lineaproduzione,Barcodetimeout = $timeout,Descrizione='$descrizione',WHERE IDStazione = $idstazione";
 if(mysql_query($query)){
 echo "updated";}
 else{
