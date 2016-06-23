b9873a513acf76a29eb3fd1577d9a36e28009aa9
amreo <amreo@live.it>
2016-06-23 16:59:12 +0200

    Spostato Sito aggiornato in sito remoto

--- Impostazioni/Sito Modifica Remoto/nuovastazione.php
+++ Impostazioni/Sito Modifica Remoto/nuovastazione.php
@@ -1,14 +1,21 @@
 <?php
 mysql_connect("127.0.0.1", "root", "PASSWORD") or die("Connection Failed");
+
 mysql_select_db("dati_produzione")or die("Connection Failed");
+
 $idstazione = $_POST['idstazione'];
+
 $lineaproduzione = $_POST['lineaproduzione'];
+
 $timeout = $_POST['timeout'];
-$descrizione = $_POST['descrizione'];
-$query = "INSERT INTO settings (IDstazione, Lineaproduzione, Barcodetimeout,Descrizione)
-VALUES ($idstazione,$lineaproduzione,$timeout,'$descrizione')";
-if(mysql_query($query)){
+
+$query = "INSERT INTO dati_produzione.settings (IDstazione, Lineaproduzione, Barcodetimeout)
+VALUES ($idstazione,$lineaproduzione,$timeout)";
+
+if(mysql_query($query))
+{
 echo "updated";}
+
 else{
 echo "fail";}
 ?>
--- Impostazioni/Sito Modifica Remoto/update.php
+++ Impostazioni/Sito Modifica Remoto/update.php
@@ -1,11 +1,16 @@
 <?php
 mysql_connect("127.0.0.1", "root", "PASSWORD") or die("Connection Failed");
+
 mysql_select_db("dati_produzione")or die("Connection Failed");
+
 $idstazione = $_POST['idstazione'];
+
 $lineaproduzione = $_POST['lineaproduzione'];
+
 $timeout = $_POST['timeout'];
-$descrizione = $_POST['descrizione'];
-$query = "UPDATE dati_produzione.settings SET Lineaproduzione = $lineaproduzione, Barcodetimeout = $timeout, Descrizione='$descrizione' WHERE IDstazione = $idstazione;";
+
+
+$query = "UPDATE dati_produzione.settings SET Lineaproduzione = $lineaproduzione, Barcodetimeout = $timeout WHERE IDstazione = $idstazione;";
 if(mysql_query($query)){
 echo "updated";}
 else{
