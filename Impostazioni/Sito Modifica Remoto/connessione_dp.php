b390f7b1d432ca20edc5b86d5d147a7f7cc239c2
kevinpre <kevin.presto@hotmail.it>
2016-06-24 10:15:34 +0200

    Update connessione_dp.php

--- Impostazioni/Sito Modifica Remoto/connessione_dp.php
+++ Impostazioni/Sito Modifica Remoto/connessione_dp.php
@@ -1,1 +1,3 @@
-< ?php include("config.php"); //includo file configurazione per recupero variabili $connessione = mysql_connect("$host","$db_user","$db_psw"); if(!$connessione) { die("Errore critico di Connessione al Database" . mysql_error()); } ?>
+<?php include("config.php");//includo file configurazione per recupero variabili
+$connessione = mysql_connect("$host","$db_user","$db_psw");
+if(!$connessione) { die("Errore critico di Connessione al Database" . mysql_error()); } ?>
