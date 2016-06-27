76e4da78052e420a36b59b37d2a2bbdc446e1b3b
amreo <amreo@live.it>
2016-06-27 11:35:53 +0200

    Fixato identazione testProtoCCS

--- testProtoCCS/Program.cs
+++ testProtoCCS/Program.cs
@@ -9,9 +9,7 @@ namespace testProtoCCS
 		public static void Main (string[] args)
 		{
 			TcpClient client = new TcpClient ();
-			client.Connect ("192.168.1.226", 1001);
-			System.IO.StreamWriter writer = new System.IO.StreamWriter (client.GetStream ());
-			System.IO.StreamReader reader = new System.IO.StreamReader (client.GetStream ());
+			client.Connect ("192.168.250.50", 1001);
 			int scelta = 99;
 			byte[] msg;
 			string sr;
@@ -24,6 +22,8 @@ namespace testProtoCCS
 				Console.WriteLine ("3) checked-add");
 				Console.WriteLine ("4) check");
 				Console.WriteLine ("5) config");
+				Console.WriteLine ("6) shutdown connessione");
+				Console.WriteLine ("7) reconnect");
 				scelta = int.Parse (Console.ReadLine ());
 
 				switch (scelta) {				
@@ -35,21 +35,20 @@ namespace testProtoCCS
 						param1 = Console.ReadLine ();
 						Console.Write ("Barcode: ");
 						param2 = Console.ReadLine ();
-					sr = string.Format ("$ADD::{0}::{1}!", param1, param2);
-					msg = System.Text.Encoding.ASCII.GetBytes (sr);
-					client.Client.Send (msg);
+						sr = string.Format ("$ADD::{0}::{1}!", param1, param2);
+						msg = System.Text.Encoding.ASCII.GetBytes (sr);
+						client.Client.Send (msg);
 						break;
-				case 2:
-					Console.Write ("Linea: ");
-					param1 = Console.ReadLine ();
-					Console.Write ("Posizione: ");
-					param2 = Console.ReadLine ();
-					Console.Write ("MSG: ");
-					param3 = Console.ReadLine ();
-					sr = string.Format ("$LOG::{0}::{1}::{2}!", param1, param2, param3);
-					msg = System.Text.Encoding.ASCII.GetBytes (sr);
-					client.Client.Send (msg);
-
+					case 2:
+						Console.Write ("Linea: ");
+						param1 = Console.ReadLine ();
+						Console.Write ("Posizione: ");
+						param2 = Console.ReadLine ();
+						Console.Write ("MSG: ");
+						param3 = Console.ReadLine ();
+						sr = string.Format ("$LOG::{0}::{1}::{2}!", param1, param2, param3);
+						msg = System.Text.Encoding.ASCII.GetBytes (sr);
+						client.Client.Send (msg);
 						break;
 					case 3:
 						Console.Write ("Linea: ");
@@ -57,43 +56,42 @@ namespace testProtoCCS
 						Console.Write ("Posizione: ");
 						param2 = Console.ReadLine ();
 						Console.Write ("Barcode: ");
-						param3 = Console.ReadLine ();
-						
-					sr = string.Format ("$CHECKED-ADD::{0}::{1}::{2}!", param1, param2, param3);
-					msg = System.Text.Encoding.ASCII.GetBytes (sr);
-					client.Client.Send (msg);
+						param3 = Console.ReadLine ();			
+						sr = string.Format ("$CHECKED-ADD::{0}::{1}::{2}!", param1, param2, param3);
+						msg = System.Text.Encoding.ASCII.GetBytes (sr);
+						client.Client.Send (msg);
 						break;
 
 					case 4:
 						Console.Write ("Barcode: ");
 						param1 = Console.ReadLine ();
-						
-					sr = string.Format ("$CHECK::{0}!", param1);
-					msg = System.Text.Encoding.ASCII.GetBytes (sr);
-					client.Client.Send (msg);
-					//TODO: TOFIX
+							
+						sr = string.Format ("$CHECK::{0}!", param1);
+						msg = System.Text.Encoding.ASCII.GetBytes (sr);
+						client.Client.Send (msg);
+						//TODO: TOFIX
 
-					while (client.Available == 0) { }
+						while (client.Available == 0) { }
 
-					msg = new byte[client.Client.Available];
-					client.Client.Receive (msg);
-					sr = System.Text.Encoding.ASCII.GetString (msg);
-					Console.WriteLine(sr);
+						msg = new byte[client.Client.Available];
+						client.Client.Receive (msg);
+						sr = System.Text.Encoding.ASCII.GetString (msg);
+						Console.WriteLine(sr);
 						break;
 					case 5:
 						Console.Write ("IDStazione: ");
 						param1 = Console.ReadLine ();
 						
-					sr = string.Format ("$CONFIG::{0}!", param1);
-					msg = System.Text.Encoding.ASCII.GetBytes (sr);
-					client.Client.Send (msg);
-					//todo:tofix
-					while (client.Available == 0) { }
+						sr = string.Format ("$CONFIG::{0}!", param1);
+						msg = System.Text.Encoding.ASCII.GetBytes (sr);
+						client.Client.Send (msg);
+						//todo:tofix
+						while (client.Available == 0) { }
 
-					msg = new byte[client.Client.Available];
-					client.Client.Receive (msg);
-					sr = System.Text.Encoding.ASCII.GetString (msg);
-					Console.WriteLine(sr);
+						msg = new byte[client.Client.Available];
+						client.Client.Receive (msg);
+						sr = System.Text.Encoding.ASCII.GetString (msg);
+						Console.WriteLine(sr);
 						break;
 				}
 
