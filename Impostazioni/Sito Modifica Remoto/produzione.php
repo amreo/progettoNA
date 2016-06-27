c3ce9e46b500f3f087faa27d2e401da67de15d29
kevinpre <kevin.presto@hotmail.it>
2016-06-27 14:05:37 +0200

    Aggiunto supporto pg

--- Impostazioni/Sito Modifica Remoto/produzione.php
+++ Impostazioni/Sito Modifica Remoto/produzione.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//se non c'è la sessione registrata
+//se non c'Ã¨ la sessione registrata
 if (!session_is_registered('autorizzato')) {
   echo "<h1>Area riservata, accesso negato.</h1>";
   echo "Per effettuare il login clicca <a href='index.php'><font color='blue'>qui</font></a>";
@@ -21,7 +21,10 @@ $cod = $_SESSION['cod']; //id cod recuperato nel file di verifica
 </head>
  
 <body>
- 
+<?php include("radioButton.php");
+if ($mysql_status = 'checked')
+{
+
 <?php 
 //connect to mysql server with host,username,password
 //if connection fails stop further execution and show mysql error
@@ -64,6 +67,55 @@ if(mysql_num_rows($query)>0):
 else: ?>
 <h3>No Results found.</h3>
 <?php endif; ?>
+
+
+}
+
+if ($pgsql_status = 'checked')
+{
+<?php 
+//connect to mysql server with host,username,password
+$conn_string = "host=127.0.0.1 port=3360 dbname=dati_produzione.output_catena user=root password=PASSWORD";
+$conn = pg_connect($conn_string);
+$query = "SELECT * FROM dati_produzione.output_catena";
+pg_query($conn, $query);
+ 
+ 
+//if we get any results we show them in table data
+if(pg_num_rows($query)>0):
+ 
+?>
+ 
+<table>
+  <tr>
+    <td align="center">ID Stazione</td>
+    <td align="center">Barcode Prodotto</td>
+    <td align="center">Nome Prodotto</td>
+    <td align="center">Descrizione</td>
+    <td align="center">Quantita'</td>
+  </tr>
+  <?php 
+  // looping 
+  while($row=pg_fetch_object($query)):?>
+  <tr>
+    <td align="center"><?php echo $row->IDstazione;  //row id ?></td>
+    <td align="center"><?php echo $row->ID_prodotto; // row first name ?></td>
+    <td align="center"><?php echo $row->Nome; //row last name  ?></td>
+    <td align="center"><?php echo $row->Descrizione; //row contact number ?></td>
+    <td align="center"><?php echo $row->numProdotti; //row created time ?></td>
+  </tr>
+  <?php endwhile;?>
+</table>
+<?php 
+// no result show 
+else: ?>
+<h3>No Results found.</h3>
+<?php endif; ?>
+
+
+
+} 
+
 <br></br>
 <a href="logout.php">Logout</a>
 </body>
