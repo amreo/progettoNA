fb1dc1824fb57c5c9b74ca4042c6d2aec75289e6
kevinpre <kevin.presto@hotmail.it>
2016-06-28 14:42:02 +0200

    Aggiunto pulsante modifica prodotto

--- Impostazioni/Sito Modifica Remoto/homepage.php
+++ Impostazioni/Sito Modifica Remoto/homepage.php
@@ -1,6 +1,3 @@
-
-
-
 <html>
   <link rel="stylesheet" href="stileindex.css" type="text/css" />
 <head>
@@ -19,14 +16,12 @@
   cursor: pointer;
   margin: 5px;
 }
-
 .button span {
   cursor: pointer;
   display: inline-block;
   position: relative;
   transition: 0.5s;
 }
-
 .button span:after {
   content: '»';
   position: absolute;
@@ -35,11 +30,9 @@
   right: -20px;
   transition: 0.5s;
 }
-
 .button:hover span {
   padding-right: 25px;
 }
-
 .button:hover span:after {
   opacity: 1;
   right: 0;
@@ -59,14 +52,12 @@
   cursor: pointer;
   margin: 5px;
 }
-
 .button1 span {
   cursor: pointer;
   display: inline-block;
   position: relative;
   transition: 0.5s;
 }
-
 .button1 span:after {
   content: '»';
   position: absolute;
@@ -75,16 +66,13 @@
   right: -20px;
   transition: 0.5s;
 }
-
 .button1:hover span {
   padding-right: 25px;
 }
-
 .button1:hover span:after {
   opacity: 1;
   right: 0;
 }
-
 .button2 {
   display: inline-block;
   border-radius: 4px;
@@ -99,14 +87,12 @@
   cursor: pointer;
   margin: 5px;
 }
-
 .button2 span {
   cursor: pointer;
   display: inline-block;
   position: relative;
   transition: 0.5s;
 }
-
 .button2 span:after {
   content: '»';
   position: absolute;
@@ -115,16 +101,13 @@
   right: -20px;
   transition: 0.5s;
 }
-
 .button2:hover span {
   padding-right: 25px;
 }
-
 .button2:hover span:after {
   opacity: 1;
   right: 0;
 }
-
 .button3 {
   display: inline-block;
   border-radius: 4px;
@@ -139,14 +122,12 @@
   cursor: pointer;
   margin: 5px;
 }
-
 .button3 span {
   cursor: pointer;
   display: inline-block;
   position: relative;
   transition: 0.5s;
 }
-
 .button3 span:after {
   content: '»';
   position: absolute;
@@ -155,16 +136,13 @@
   right: -20px;
   transition: 0.5s;
 }
-
 .button3:hover span {
   padding-right: 25px;
 }
-
 .button3:hover span:after {
   opacity: 1;
   right: 0;
 }
-
 .button4 {
   display: inline-block;
   border-radius: 4px;
@@ -179,14 +157,12 @@
   cursor: pointer;
   margin: 5px;
 }
-
 .button4 span {
   cursor: pointer;
   display: inline-block;
   position: relative;
   transition: 0.5s;
 }
-
 .button4 span:after {
   content: '»';
   position: absolute;
@@ -195,40 +171,78 @@
   right: -20px;
   transition: 0.5s;
 }
-
 .button4:hover span {
   padding-right: 25px;
 }
-
 .button4:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+ 
+.button5 {
+  display: inline-block;
+  border-radius: 4px;
+  background-color:  #00cc7a;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 28px;
+  padding: 20px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+.button5 span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+.button5 span:after {
+  content: '»';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+.button5:hover span {
+  padding-right: 25px;
+}
+.button5:hover span:after {
   opacity: 1;
   right: 0;
 }
 </style>
 </head>
 <body>
-
+<section id = "sinistra">
 <h2>Gestione Stazioni</h2>
 
 <a id="btn" href="impostazioni.php"<button class="button" style="vertical-align:middle"><span>Modifica</span></button></a>
   <br></br>
 <a id="btn" href="nuova.php" <button class="button1" style="vertical-align:middle"><span>Nuova</span></button></a>
 <br></br>
-
+</section>
+<section id = "centro">
 <h2>Gestione Prodotti</h2>
 
+
+<a id="btn" href="modificaprodotti.php" <button class="button5" style="vertical-align:middle"><span>Modifica</span></button></a>
+<br></br>
 <a id="btn" href="prodotti.php" <button class="button2" style="vertical-align:middle"><span>Nuovo</span></button></a>
+ <br></br>
+</section>
+<section id = "destra">
+<h2>Visualizza Database</h2>
+
+<a id="btn" href="produzione.php" <button class="button3" style="vertical-align:middle"><span>Produzione</span></button></a>
 
 <br></br>
 
-<h2>Visualizza Produzione</h2>
-
-<a id="btn" href="produzione.php" <button class="button3" style="vertical-align:middle"><span>Vedi</span></button></a>
-
-<h2>Visualizza Log Eventi</h2>
-
-<a id="btn" href="log.php" <button class="button4" style="vertical-align:middle"><span>Vedi</span></button></a>
-
+<a id="btn" href="log.php" <button class="button4" style="vertical-align:middle"><span>Log Eventi</span></button></a>
+</section>
 <br></br>
 <a id="logout" href="logout.php">Logout</a>
 
