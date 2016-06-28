58953280c0b27c0d2b94b4e49beb8f2cec5f5405
kevinpre <kevin.presto@hotmail.it>
2016-06-28 12:08:58 +0200

    Update homepage.php

--- Impostazioni/Sito Modifica Remoto/homepage.php
+++ Impostazioni/Sito Modifica Remoto/homepage.php
@@ -210,24 +210,24 @@
 
 <h2>Gestione Stazioni</h2>
 
-<a  href="impostazioni.php"<button class="button" style="vertical-align:middle"><span>Modifica</span></button></a>
+<a id="btn" href="impostazioni.php"<button class="button" style="vertical-align:middle"><span>Modifica</span></button></a>
   <br></br>
-<a href="nuova.php" <button class="button1" style="vertical-align:middle"><span>Nuova</span></button></a>
+<a id="btn" href="nuova.php" <button class="button1" style="vertical-align:middle"><span>Nuova</span></button></a>
 <br></br>
 
 <h2>Gestione Prodotti</h2>
 
-<a href="prodotti.php" <button class="button2" style="vertical-align:middle"><span>Nuovo</span></button></a>
+<a id="btn" href="prodotti.php" <button class="button2" style="vertical-align:middle"><span>Nuovo</span></button></a>
 
 <br></br>
 
 <h2>Visualizza Produzione</h2>
 
-<a href="produzione.php" <button class="button3" style="vertical-align:middle"><span>Vedi</span></button></a>
+<a id="btn" href="produzione.php" <button class="button3" style="vertical-align:middle"><span>Vedi</span></button></a>
 
 <h2>Visualizza Log Eventi</h2>
 
-<a href="log.php" <button class="button4" style="vertical-align:middle"><span>Vedi</span></button></a>
+<a id="btn" href="log.php" <button class="button4" style="vertical-align:middle"><span>Vedi</span></button></a>
 
 <br></br>
 <a id="logout "href="logout.php">Logout</a>
