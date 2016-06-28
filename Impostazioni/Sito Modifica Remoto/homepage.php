68ca193b33216736c2a12032d80d172d88773c78
kevinpre <kevin.presto@hotmail.it>
2016-06-28 10:21:16 +0200

    Update homepage.php

--- Impostazioni/Sito Modifica Remoto/homepage.php
+++ Impostazioni/Sito Modifica Remoto/homepage.php
@@ -1,16 +1,4 @@
-<?php
-session_start();
-//se non c'è la sessione registrata
-if (!session_is_registered('autorizzato')) {
-  echo "<h1>Area riservata, accesso negato.</h1>";
-  echo "Per effettuare il login clicca <a href='index.php'><font color='blue'>qui</font></a>";
-  die;
-}
- 
-//Altrimenti Prelevo il codice identificatico dell'utente loggato
-session_start();
-$cod = $_SESSION['cod']; //id cod recuperato nel file di verifica
-?>
+
 
 
 <html>
