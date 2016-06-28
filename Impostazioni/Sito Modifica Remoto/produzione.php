fbfb12004508095bb911f36f9736ebf7f4bc52d7
kevinpre <kevin.presto@hotmail.it>
2016-06-28 10:40:06 +0200

    Update produzione.php

--- Impostazioni/Sito Modifica Remoto/produzione.php
+++ Impostazioni/Sito Modifica Remoto/produzione.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html >
 <html >
 <head>
-<meta charset="utf-8" />
+<meta charset="ISO-8859-1">
  <link href="stileproduzione.css" rel="stylesheet" type="text/css">
 <title></title>
 </head>
