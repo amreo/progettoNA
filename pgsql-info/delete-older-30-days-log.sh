eae7d63119dca9bce666e57119517152cc1cef5d
amreo <amreo@live.it>
2016-06-29 15:25:19 +0200

    aggiunto script di cancellazione righe più vecchie di 30gg

--- pgsql-info/delete-older-30-days-log.sh
+++ pgsql-info/delete-older-30-days-log.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-psql -qd dati_produzione -c "DELETE FROM dati_produzione.log_eventi WHERE Date < DATE_SUB( CURRENT_TIME(), INTERVAL 30 DAY )"
-psql -qd dati_produzione -c "DELETE FROM dati_produzione.log_produzione WHERE Date < DATE_SUB( CURRENT_TIME(), INTERVAL 30 DAY )"
+/usr/bin/psql -qd dati_produzione -c "DELETE FROM dati_produzione.log_eventi WHERE Date < DATE_SUB( CURRENT_TIME(), INTERVAL 30 DAY )"
+/usr/bin/psql -qd dati_produzione -c "DELETE FROM dati_produzione.log_produzione WHERE Date < DATE_SUB( CURRENT_TIME(), INTERVAL 30 DAY )"
