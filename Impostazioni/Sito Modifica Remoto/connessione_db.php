d341114054650cab7731247c7b287fe777fdc682
kevinpre <kevin.presto@hotmail.it>
2016-06-29 15:06:28 +0200

    Fix minore sintassi

--- Impostazioni/Sito Modifica Remoto/connessione_db.php
+++ Impostazioni/Sito Modifica Remoto/connessione_db.php
@@ -1,4 +1,4 @@
 <?php include("config.php");//includo file configurazione per recupero variabili
-$connessione = mysql_connect("$host","$db_user","$db_psw");
+$connessione = pg_connect("$host","$db_user","$db_psw");
 if(!$connessione) { die("Errore critico di Connessione al Database" . mysql_error()); } ?>
 echo "ciao e test";
