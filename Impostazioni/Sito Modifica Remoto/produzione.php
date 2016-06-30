0e2979aa77654a92dff3f9adeb5b010c8c4f25b3
kevinpre <kevin.presto@hotmail.it>
2016-06-30 12:19:57 +0200

    Update produzione.php

--- Impostazioni/Sito Modifica Remoto/produzione.php
+++ Impostazioni/Sito Modifica Remoto/produzione.php
@@ -8,58 +8,6 @@
  
 <body>
 <?php include("radioButton.php");
-if ($mysql_status = 'checked')
-{
-
-<?php 
-header('Content-Type: text/html; charset=utf-8');
-//connect to mysql server with host,username,password
-//if connection fails stop further execution and show mysql error
-$connection=mysql_connect('localhost','root','PASSWORD') or die(mysql_error());
-//select a database for given connection
-//if database selection  fails stop further execution and show mysql error
-mysql_select_db('dati_produzione',$connection) or die(mysql_error());
- 
-mysql_query("SET character_set_results=utf8;") or die(mysql_error());
-
-//execute a mysql query to retrieve all the users from users table
-//if  query  fails stop further execution and show mysql error
-$query=mysql_query("SELECT * FROM dati_produzione.output_catena;") or die(mysql_error());
- 
-//if we get any results we show them in table data
-if(mysql_num_rows($query)>0):
- 
-?>
- 
-<table>
-  <tr>
-    <th align="center">ID Stazione</th>
-    <th align="center">Barcode Prodotto</th>
-    <th align="center">Nome Prodotto</th>
-    <th align="center">Descrizione</th>
-    <th align="center">Quantita'</th>
-  </tr>
-  <?php 
-  // looping 
-  while($row=mysql_fetch_object($query)):?>
-  <tr>
-    <td align="center"><?php echo $row->IDstazione;  //row id ?></td>
-    <td align="center"><?php echo $row->ID_prodotto; // row first name ?></td>
-    <td align="center"><?php echo $row->Nome; //row last name  ?></td>
-    <td align="center"><?php echo $row->Descrizione; //row contact number ?></td>
-    <td align="center"><?php echo $row->numProdotti; //row created time ?></td>
-  </tr>
-  <?php endwhile;?>
-</table>
-<?php 
-// no result show 
-else: ?>
-<h3>No Results found.</h3>
-<?php endif; ?>
-
-
-}
-
 if ($pgsql_status = 'checked')
 {
 <?php 
