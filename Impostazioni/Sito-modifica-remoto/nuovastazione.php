e585c3592d46fc71a19f1a765e504d40c3d4fc95
amreo <amreo@live.it>
2016-07-01 20:45:43 +0200

    identato codice

--- Impostazioni/Sito-modifica-remoto/nuovastazione.php
+++ Impostazioni/Sito-modifica-remoto/nuovastazione.php
@@ -1,26 +1,22 @@
 <?php
-session_start();
-mysql_connect("127.0.0.1", "root", "PASSWORD") or die("Connection Failed");
+	session_start();
+	mysql_connect("127.0.0.1", "root", "PASSWORD") or die("Connection Failed");
+	mysql_select_db("dati_produzione")or die("Connection Failed");
 
-mysql_select_db("dati_produzione")or die("Connection Failed");
+	$idstazione = $_POST['idstazione'];
+	$lineaproduzione = $_POST['lineaproduzione'];
+	$timeout = $_POST['timeout'];
 
-$idstazione = $_POST['idstazione'];
+	$_SESSION["success2"]= 0;
 
-$lineaproduzione = $_POST['lineaproduzione'];
+	$query = "INSERT INTO dati_produzione.settings (IDstazione, Lineaproduzione, Barcodetimeout)
+	VALUES ($idstazione,$lineaproduzione,$timeout)";
 
-$timeout = $_POST['timeout'];
-
-$_SESSION["success2"]= 0;
-
-$query = "INSERT INTO dati_produzione.settings (IDstazione, Lineaproduzione, Barcodetimeout)
-VALUES ($idstazione,$lineaproduzione,$timeout)";
-
-if(mysql_query($query))
-{
-echo "<script language=javascript>window.location.href='homepage.php';</script>";
-  $_SESSION["success2"]= 1;
-}
-
-else{
-echo "Operazione fallita";}
+	if(mysql_query($query))
+	{
+		echo "<script language=javascript>window.location.href='homepage.php';</script>";
+		$_SESSION["success2"]= 1;
+	} else {
+		echo "Operazione fallita";
+	}
 ?>
--- Impostazioni/Sito-modifica-remoto/verifica.php
+++ Impostazioni/Sito-modifica-remoto/verifica.php
@@ -1,46 +1,42 @@
 <?php
-session_start(); //inizio la sessione
-//includo i file necessari a collegarmi al db con relativo script di accesso
-include("connessione_db.php");
-include("config.php"); 
- 
-//mi collego
-mysql_select_db("$db_name",$connessione); 
+	session_start(); //inizio la sessione
+	//includo i file necessari a collegarmi al db con relativo script di accesso
+	include("connessione_db.php");
+	include("config.php"); 
 
- 
-//variabili POST con anti sql Injection
-$username=mysql_real_escape_string($_POST['username']); //faccio l'escape dei caratteri dannosi
-$password=mysql_real_escape_string(sha1($_POST['password'])); //sha1 cifra la password anche qui in questo modo corrisponde con quella del db
- 
- $query = "SELECT * FROM dati_produzione.login WHERE username = '$username' AND password = '$password' ";
- $ris = mysql_query($query, $connessione) or die (mysql_error());
- $riga=mysql_fetch_array($ris);  
- 
-/*Prelevo l'identificativo dell'utente */
-$cod=$riga['username'];
- 
-/* Effettuo il controllo */
-if ($cod == NULL) $trovato = 0 ;
-else $trovato = 1;  
- 
-/* Username e password corrette */
-if($trovato == 1) {
- 
- /*Registro la sessione*/
-  //session_register("autorizzato");
- 
-  $_SESSION["autorizzato"] = 1;
- 
-  /*Registro il codice dell'utente*/
-  $_SESSION['cod'] = $cod;
- 
- /*Redirect alla pagina riservata*/
-   echo "<script language=javascript>window.location.href='homepage.php';</script>"; 
- 
-} else {
- 
-/*Username e password errati, redirect alla pagina di login*/
- echo '<script language=javascript>window.location.href="index.php"</script>';
- 
-}
+	//mi collego
+	mysql_select_db("$db_name",$connessione); 
+
+	//variabili POST con anti sql Injection
+	$username=mysql_real_escape_string($_POST['username']); //faccio l'escape dei caratteri dannosi
+	$password=mysql_real_escape_string(sha1($_POST['password'])); //sha1 cifra la password anche qui in questo modo corrisponde con quella del db
+	$query = "SELECT * FROM dati_produzione.login WHERE username = '$username' AND password = '$password' ";
+	$ris = mysql_query($query, $connessione) or die (mysql_error());
+	$riga=mysql_fetch_array($ris);  
+
+	/*Prelevo l'identificativo dell'utente */
+	$cod=$riga['username'];
+
+	/* Effettuo il controllo */
+	if ($cod == NULL)
+		$trovato = 0;
+	else 
+		$trovato = 1;  
+
+	/* Username e password corrette */
+	if($trovato == 1)
+	{
+		/*Registro la sessione*/
+		//session_register("autorizzato");
+		$_SESSION["autorizzato"] = 1;
+		/*Registro il codice dell'utente*/
+		$_SESSION['cod'] = $cod;
+
+		/*Redirect alla pagina riservata*/
+		echo "<script language=javascript>window.location.href='homepage.php';</script>"; 
+	} else {
+		/*Username e password errati, redirect alla pagina di login*/
+		echo '<script language=javascript>window.location.href="index.php";</script>';
+
+	}
 ?>
