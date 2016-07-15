5e2b33978d315768be4692e3a4ff13e9a84a190e
IAgronautI <IAgronautI@users.noreply.github.com>
2016-07-15 11:06:03 +0200

    Update stampa.php

--- Impostazioni/Sito-modifica-remoto/stampa.php
+++ Impostazioni/Sito-modifica-remoto/stampa.php
@@ -1,1 +1,14 @@
-bla
+<?php
+require('fpdf.php');
+$prodotto=$_post['prodotto'];
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',60);
+
+$pdf->Text(20,50,$prodotto);
+$pdf->Text(20,90,"ID PRODOTTO = 3");
+$pdf->Text(20,130,"BARCODE:");
+$pdf->Image("Alimentatore.jpg",20,160,180);
+$pdf->Output();
+?>
