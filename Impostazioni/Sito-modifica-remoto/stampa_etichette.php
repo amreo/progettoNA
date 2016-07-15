68b48c39426c60ed9fe672bc4f7151d91b5c4acd
IAgronautI <IAgronautI@users.noreply.github.com>
2016-07-15 10:39:45 +0200

    Update stampa_etichette.php

--- Impostazioni/Sito-modifica-remoto/stampa_etichette.php
+++ Impostazioni/Sito-modifica-remoto/stampa_etichette.php
@@ -1,21 +1,25 @@
-<?php
-	session_start();
-	if($_SESSION["autorizzato"] == 0)
-	{
-		echo '<script language=javascript>window.location.href="nonautorizzato.html"</script>';
-	}
-?>
-
-<html>
-	<link rel="stylesheet" href="stile.css" type="text/css" />
-	<form method="post" name="Update" action="stampa.php" />
-		<h3>Stampa</h3>
-		
-		<p>Prodotto<p>
-		<input type="text" name="idstazione" />
-		<br></br>
-		<input type="submit" name="Update" value="Stampa"/>
-		<br></br>
-		<a href="homepage.php">Home</a>
-	</form> 
-</html>
+	<?php
+		session_start();
+		if($_SESSION["autorizzato"] == 0)
+		{
+			echo '<script language=javascript>window.location.href="nonautorizzato.html"</script>';
+		}
+	?>
+	
+	<html>
+		<link rel="stylesheet" href="stile.css" type="text/css" />
+		<form method="post" name="Update" action="stampa.php" />
+			<h3>Stampa</h3>
+			
+			<p>Prodotto<p>
+			<select name="prodotti" >
+	   			<option value="Mouse">Mouse</option></option>
+	   			<option value="Alimentatore">Alimentatore</option>
+	   			<option value="Tastiera">Tastiera</option>
+	  		</select>
+			<br></br>
+			<input type="submit" name="Update" value="Stampa"/>
+			<br></br>
+			<a href="homepage.php">Home</a>
+		</form> 
+	</html>
