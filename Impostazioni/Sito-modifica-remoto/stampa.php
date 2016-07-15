783eacd7c570347382ea62928dfe2e780de9f5e5
IAgronautI <IAgronautI@users.noreply.github.com>
2016-07-15 11:32:30 +0200

    Update stampa.php

--- Impostazioni/Sito-modifica-remoto/stampa.php
+++ Impostazioni/Sito-modifica-remoto/stampa.php
@@ -1,13 +1,15 @@
 <?php
 require('fpdf.php');
-
+$prodotto=$_post["prodotto"]
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',60);
 
-$pdf->Text(20,50,"ALIMENTATORE");
-$pdf->Text(20,90,"ID PRODOTTO = 3");
-$pdf->Text(20,130,"BARCODE:");
+$pdf->Text(20,50,$prodotto);
+$pdf->Text(20,100,"BARCODE:");
+
+//INSERIRE IMMAGINE BARCODE
+
 $pdf->Output();
 ?>
