8c14df7dfa3d6e172f32e95525c510246adc38f8
faalcoo <ddflc99@gmail.com>
2016-07-15 10:27:29 +0200

    Update stampa_etichette.php

--- Impostazioni/Sito-modifica-remoto/stampa_etichette.php
+++ Impostazioni/Sito-modifica-remoto/stampa_etichette.php
@@ -7,14 +7,22 @@
 ?>
 
 <html>
-	<link rel="stylesheet" href="stilestampaetichette.css" type="text/css" />
-	<form method="post" name="Stampa" action="stampa.php">
-		<h3>Stampa</h3>
-		<form> 
-  			<select name='prodotti'>
-				 
-  			</select>
+	<link rel="stylesheet" href="stile.css" type="text/css" />
+	<form method="post" name="Update" action="updatestazione.php" />
+		<h3>Impostazioni</h3>
+		
+		<p>ID Stazione<p>
+		<input type="text" name="idstazione" />
+		
+		<p>Linea Produzione<p>
+		<input type="text" name="lineaproduzione" />
+		
+		<p>Timeout Barcode<p>
+ 		<input type="text" name="timeout" />
+		
+		<br></br>
+		<input type="submit" name="Submit" value="Update" />
+		<br></br>
 		<a href="homepage.php">Home</a>
-	        </form> 
 	</form> 
 </html>
